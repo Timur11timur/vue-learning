1895228eed7d95f59bb44d1405d0e60d8ad1b81b
Timur <t.fralik@aimprosoft.com>
2022-09-20 23:52:01 +0300

    Add composable for using local storage

--- my-vue-project/src/views/ContactView.vue
+++ my-vue-project/src/views/ContactView.vue
@@ -1,7 +1,19 @@
+<script setup>
+import { useStorage } from "@/composables/useStorage";
+
+let food = useStorage('food');
+let age = useStorage('age', 21);
+</script>
+
 <template>
-  <div class="contact">
-    <h1>This is a contact page</h1>
-  </div>
+  <main>
+    <p>
+      What is your favorite food? <input type="text" v-model="food" @input="write('food', food)">
+    </p>
+    <p>
+      How old are you? <input type="text" v-model="age" @input="write('age', age)">
+    </p>
+  </main>
 </template>
 
 <style>
