7fcc6dbdf334d4704ffdc38a65039dbf07601155
Timur <t.fralik@aimprosoft.com>
2022-09-21 12:29:43 +0300

    Add tabbable textarea component

---
+++ my-vue-project/src/views/FormView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TabbableTextarea from "@/components/TabbableTextarea.vue";
+import {ref} from "vue";
+
+let commit = ref('Default text');
+
+</script>
+
+<template>
+  <main>
+    <form action="">
+      <TabbableTextarea v-model="commit"/>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 300px;
+}
+</style>
